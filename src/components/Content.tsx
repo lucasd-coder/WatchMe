c2aec0efeb92e9903feb9adbac2383faf3718386
Lucas da Silva <lucasdasilva68@yahoo.com>
2021-06-19 18:20:29 -0300

    components Content, SideBar

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,43 @@
-export function Content() {
-  // Complete aqui
+
+import "../styles/content.scss";
+import { MovieCard } from "./MovieCard";
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  title: string;
+  subtitle: string;
+  component: MovieProps[];
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">{props.title}<span>{props.subtitle}</span></span>        
+      </header>
+      <main>
+          <div className="movies-list">
+            {props.component.map(movie => (
+              <MovieCard 
+                key={movie.imdbID}
+                title={movie.Title}
+                poster={movie.Poster}
+                runtime={movie.Runtime}
+                rating={movie.Ratings[0].Value}
+              />
+            ))}
+          </div>
+      </main>
+    </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,18 @@
-export function SideBar() {
-  // Complete aqui
+import "../styles/sidebar.scss";
+
+interface SidebarProps {
+  title: string
+  subtitle: string
+  component: any;
+}
+
+export function SideBar(props: SidebarProps) {
+  return (
+    <nav className="sidebar">
+      <span>{props.title}<p>{props.subtitle}</p></span>
+      <div className="buttons-container">
+        {props.component}
+      </div>
+    </nav>
+  )
 }
